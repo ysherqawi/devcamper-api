21d0bec3235bb913baa7e5b918d44814292fa944
Yasser <Yasser@Git.com>
2020-05-09 01:53:28 +0300

    Add Dev logging middleware

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const morgan = require('morgan');
 const dotenv = require('dotenv');
 
+//Route files
+const bootcamps = require('./routes/bootcamps');
+
+//Load env var
 dotenv.config({
   path: './config/config.env',
 });
 
 const app = express();
 
-const bootcamps = require('./routes/bootcamps');
+//Dev logging middleware
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
+//Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
