372d4e494a4956c23cd3865d7380e5db2fce2657
Yasser <Yasser@Git.com>
2020-05-10 01:03:16 +0300

    Error handling & async handler

--- startup/routes.js
+++ startup/routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bootcamps = require('../routes/bootcamps');
+const errorHandler = require('../middleware/error');
 
 module.exports = function (app) {
   app.use(express.json());
   app.use('/api/v1/bootcamps', bootcamps);
+  app.use(errorHandler);
 };
