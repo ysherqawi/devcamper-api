1d03e03582fb131ab9c9c3e298dff7fb67e5ce2e
Yasser <Yasser@Git.com>
2020-05-12 01:19:37 +0300

    Advanced results middleware

--- controllers/courses.js
+++ controllers/courses.js
@@ -8,29 +8,20 @@ const asyncHandler = require('../middleware/async');
 // @route   GET /api/v1/bootcamps/:bootcampId/courses
 // @access  Public
 exports.getCourses = asyncHandler(async (req, res, next) => {
-  let query;
+  if (req.params.bootcampId) {
+    const courses = await Course.find({ bootcamp: req.params.bootcampId });
 
-  if (req.params.bootcampId)
-    query = Course.find({ bootcamp: req.params.bootcampId });
-  else
-    query = Course.find().populate({
-      path: 'bootcamp',
-      select: 'name description',
-    });
-
-  const courses = await query;
-
-  res.status(200).json({ success: true, count: courses.length, data: courses });
+    res
+      .status(200)
+      .json({ success: true, count: courses.length, data: courses });
+  } else res.status(200).json(res.advancedResults);
 });
 
 // @desc    Get single course
 // @route   GET /api/v1/courses/:id
 // @access  Public
 exports.getCourse = asyncHandler(async (req, res, next) => {
-  const course = await Course.findById(req.params.id).populate({
-    path: 'bootcamp',
-    select: 'name description',
-  });
+  const course = await Course.findById(req.params.id);
 
   if (!course)
     return next(
