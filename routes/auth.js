f0b8a1d1f40a4b45ff64d837f3ea73ebcd7d06c2
Yasser <Yasser@Git.com>
2020-05-15 01:40:55 +0300

    Added logout to clear cookie and add comments in auth middleware

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {
   register,
   login,
+  logout,
   getMe,
   updateDetails,
   updatePassword,
@@ -16,6 +17,8 @@ const router = express.Router();
 router.route('/register').post(register);
 
 router.route('/login').post(login);
+
+router.route('/logout').get(logout);
 
 router.route('/me').get(protect, getMe);
 
